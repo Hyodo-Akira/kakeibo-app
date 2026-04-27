f6c797440096662326c3172fe9a945ef20f0b8ef
AkiraHyodo <a30.a12.f21@gmail.com>
2026-04-28 07:32:11 +0900

    CSSの適用

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Text } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
     const items = [
@@ -7,10 +7,29 @@ export default function App() {
         { id: '3', name: '娯楽費', amount: 1000 },
     ];
     return (
-        <FlatList
-            data={items}
-            renderItem={({ item }) => <Text>{item.name}:{item.amount}円</Text>}
-            keyExtractor={(item) => item.id}
-        />
+        <View style={Styles.container}>
+            <Text style={Styles.title}>家計簿</Text>
+            <FlatList
+                data={items}
+                renderItem={({ item }) => <Text style={Styles.items}>{item.name}:{item.amount}円</Text>}
+                keyExtractor={(item) => item.id}
+            />
+        </View>
     )
 }
+
+const Styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+        padding: 16
+    },
+    title: {
+        fontSize: 24,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    items: {
+        padding: 10
+    }
+})
